3a4c1c56292db14cde0c153d755bb996fe56f3d8
CecilMkhari(LubanziA <CecilMkhari(LubanziA@DESKTOP-8DS478L>
2025-05-20 14:28:57 +0200

    Add project files.

---
+++ EmployeeApp/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp
+{
+    public class Person
+    {
+
+        // Public property to hold a unique identifier for an object
+        // Private field to store the name; accessible only within this class
+        // Protected field to store the surname; accessible within this class and derived classes
+        // Private field to store the salary; accessible only within this class
+        public int Id;  
+        private string _name;
+        protected string Surname;
+        private double salary;
+
+
+        // Method that takes a string parameter and returns void (no return value)
+        // It assigns the passed name to the private field _name and prints it
+        public void getName(string name)
+        {
+            _name = name;
+            Console.WriteLine(_name);
+        }
+
+        //  property for getting and setting the value of Fname
+        public string Fname { get; set; }
+
+        // Method that takes a double parameter and returns a double value
+        // It returns the value of the salary passed to it (note: doesn't use the private field 'salary')
+        public double getSalary(double salary)
+        {
+            return salary;
+        }
+
+    }
+}
---
+++ EmployeeApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an object of the class 'Person'
+            Person person = new Person();
+
+            // Prompting user for integer input for ID and assigning it to the person's Id property
+            Console.Write(" Enter id :");
+            person.Id = Convert.ToInt32(Console.ReadLine());
+
+            // Prompting user for string input for name and storing it in a local variable
+            Console.Write(" Enter name :");
+            string name = Console.ReadLine();
+
+            // Prompting user for string input for first name and assigning it to the person's Fname property
+            Console.Write(" Enter Fname :");
+            person.Fname = Console.ReadLine();
+
+            // Prompting user for double input for salary and storing it in a local variable
+            Console.Write(" Enter the salary :");
+            double salary = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\n"); // Printing a blank line for formatting
+
+            // Displaying the entered and processed information
+
+            // Displaying the ID value
+            Console.WriteLine(" ID    : " + person.Id);
+
+            // Displaying the name using the getName method (which also prints inside the method)
+            person.getName(" Name  : " + name);
+
+            // Displaying the first name
+            Console.WriteLine(" Fname : " + person.Fname);
+
+            // Displaying the salary using the getSalary method
+            Console.WriteLine(" Salary : " + person.getSalary(salary));
+
+        }
+    }
+}
